2c0989a05642f5734cb5e1fa4ebc7f3306ef9df3
shhn35 <hoseiny.hadi@gmail.com>
2021-01-25 10:19:04 +0100

    string compression - in place memory

--- src/main/java/de/shhn/Algorithm/medium/StringCompression.java
+++ src/main/java/de/shhn/Algorithm/medium/StringCompression.java
@@ -1,7 +1,7 @@
 package de.shhn.Algorithm.medium;
 
 public class StringCompression {
-    public int compress(char[] chars) {
+    public int compressOtherApproach(char[] chars) {
         Integer counter = 0;
         char curr = chars[0];
         int index = 0;
@@ -41,4 +41,46 @@ public class StringCompression {
 
         return index;
     }
+
+    public int compress(char[] chars) {
+        int counter = 0, index = 0;
+        char c = chars[0];
+        for(int i = 0; i<chars.length;i++){
+            if(chars[i] == c)
+                counter++;
+            else{
+                chars[index++] = c;
+                c = chars[i];
+                if(counter>1){
+                    index = appendCount(chars,index,counter);
+                    counter = 1;
+                }
+            }
+
+        }
+
+        // the last char
+        chars[index++] = c;
+        if(counter>1)
+            index = appendCount(chars,index,counter);
+
+        return index;
+    }
+
+    private int appendCount(char[] chars,int index,int counter){
+        int d;
+        while(counter>=0){
+            d = counter / 10 ;
+            if(d == 0){
+                // it is the last digit which is the denominator
+                d = counter;
+                counter = -1;
+            }else{
+                counter = counter % 10;
+            }
+            chars[index++] = (char)(d+'0');
+        }
+        return index;
+    }
+
 }
